15d5d60124f2c102385c20b2b185ebbc7a7f0617
BenjaminBaredes <bbaredes@gmail.com>
2026-05-18 14:22:14 -0300

    .

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+
+    UIManager uiManagerScript;
+    public bool isIntercting = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Interactable interactableScript = other.GetComponent<Interactable>();
+        if(interactableScript != null){
+            uiManagerScript.ShowCartelPresione();
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        uiManagerScript.HideCartelPresione();
+    }
+}
